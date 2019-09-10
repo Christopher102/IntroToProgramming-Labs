74348752aa536e3b6a9ca57fc71c6c81d693cbec
Christopher201 <Christopher.Fioti1@marist.edu>
2019-09-10 19:00:06 -0400

    Solutions to the Mad Libs exercise

---
+++ lab02/madlibs.py
@@ -0,0 +1,11 @@
+# Fill in the blanks Mad Libs Game
+# Enter in 4 words and it'll make a sentence!
+
+def main():
+    noun = input("Please enter a noun >")
+    verb = input("Please enter a verb >")
+    adjective = input("Please enter an adjective >")
+    place = input("Please enter a location >")
+    print("The " + noun + " " + verb + " to the " + adjective + " " + place)
+
+main()
