ed55d84dfe64bbc07003a1045e9e1e5b2f56cd0b
Christopher201 <Christopher.Fioti1@marist.edu>
2019-09-12 14:01:49 -0400

    Solutions to evens loop exercise

--- lab02/evens.py
+++ lab02/evens.py
@@ -3,8 +3,8 @@
 
 
 def main():
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    for n in list1:
+    
+    for n in range(1,21):
         if n % 2 == 0:
             print(n)
 
