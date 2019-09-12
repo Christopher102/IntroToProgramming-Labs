3b1908555423e1d7b70ffb8e91ab903b840ce095
Christopher201 <Christopher.Fioti1@marist.edu>
2019-09-12 14:04:37 -0400

    Fixed a couple small things that were showing errors

--- lab02/rover.py
+++ lab02/rover.py
@@ -1,11 +1,11 @@
 # This program will calculate how long it takes a picture from Curiosity
 # To Reach Earth from Mars's Closest Distance
 
+
 def main():
     print("Curiosity transmitts pictures to Earth in")
     A = 34000000
     B = 186000
     c = A / B
-    print(c)
-    print("Seconds")
+    print(c + "Seconds!")
 main()
